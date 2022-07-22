666b78e0e6501c29cc64e857119c82c57b4edbf9
Alfredo <caiffffan@gmail.com>
2022-07-22 11:15:54 -0500

    remover swagger

--- IaExamApi/Startup.cs
+++ IaExamApi/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+//using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -46,16 +46,16 @@ namespace IaExamApi
 								  });
 			});
 
-			services.AddSwaggerGen();
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo
-				{
-					Version = "v1",
-					Title = "Implement Swagger UI",
-					Description = "A simple example to Implement Swagger UI",
-				});
-			});
+			//services.AddSwaggerGen();
+			//services.AddSwaggerGen(c =>
+			//{
+			//	c.SwaggerDoc("v1", new OpenApiInfo
+			//	{
+			//		Version = "v1",
+			//		Title = "Implement Swagger UI",
+			//		Description = "A simple example to Implement Swagger UI",
+			//	});
+			//});
 
 			services.AddControllers();
 		}
@@ -69,10 +69,10 @@ namespace IaExamApi
 			}
 
 
-			app.UseSwagger();
-			app.UseSwaggerUI(c => {
-				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Showing API V1");
-			});
+			//app.UseSwagger();
+			//app.UseSwaggerUI(c => {
+			//	c.SwaggerEndpoint("/swagger/v1/swagger.json", "Showing API V1");
+			//});
 
 			app.UseHttpsRedirection();
 
